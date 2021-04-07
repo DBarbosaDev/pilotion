034142616177d85ea58fbf60659d0b9b32b099f1
DBarbosaDev <diogobarbosa1@outlook.pt>
2021-04-07 17:51:25 +0100

    Base commit

--- so2-pilotion-app/passag/passag.c
+++ so2-pilotion-app/passag/passag.c
@@ -4,9 +4,15 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <windows.h>
+#include <fcntl.h>
 
 int _tmain()
 {
+    #ifdef UNICODE
+        _setmode(_fileno(stdin), _O_WTEXT);
+        _setmode(_fileno(stdout), _O_WTEXT);
+        _setmode(_fileno(stderr), _O_WTEXT);
+    #endif
     wprintf(_T("Olá Mundo de passageiros"));
 }
 

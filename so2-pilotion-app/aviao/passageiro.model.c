1bb53340dd0f3b0817a6f521230c760618553531
Alexandre Reis <alex.cst.reis@gmail.com>
2021-04-29 02:50:40 +0100

    feat: added coerence with portuguese code variables
    - translated code
    - initial passanger methods and refactor of airplane structure and method

---
+++ so2-pilotion-app/aviao/passageiro.model.c
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "passageiro.model.h"
+#include "aviao.model.h"
+
+/**
+ * @inheritDoc  
+**/
+Passageiro novoPassageiro(int id, int tempoEspera, TCHAR* args[])
+{
+	Passageiro passag;
+
+	passag.id = id;
+	passag.tempoEspera = tempoEspera;
+	passag.nome = args[0];
+	passag.siglaAeroportoPartida = args[1];
+	passag.siglaAeroportoDestino = args[2];
+
+	return passag;
+}
+
